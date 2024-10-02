33c5123eb7bcf794d1dc2bf5791506dcde4dd7e3
towerwatchman <towerwatchman@gmail.com>
2024-10-02 12:57:30 -0400

    fixed  game scanner

---
+++ dnc8_test/ImportTester/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System;
+using Microsoft.VisualBasic;
+
+namespace Atlas;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "I:\\Games";
+        List<string> root_paths = new List<string>();
+
+        string[] directories = Directory.GetDirectories(path);
+
+        int total_dirs = directories.Length;
+
+        Console.WriteLine($"There are a total of {total_dirs} folders");
+
+        //We need to go through each item and find if it is a folder or file
+        foreach (string dir in directories)
+        {
+           int cur_level = 0;
+           int stop_level = 15; //Set to max of 15 levels. There should not be more than 15 at most
+           foreach(string t in Directory.GetDirectories(dir,"*", SearchOption.AllDirectories)) 
+           {
+                cur_level = t.Split('\\').Length;
+                string[] s = t.Split('\\');
+                if (cur_level <= stop_level)
+                {
+                    List<string> potential_executables = Executable.DetectExecutable(Directory.GetFiles(t));
+                    if (potential_executables.Count > 0)
+                    {
+                        //Console.WriteLine($"{t} | {cur_level} <= {stop_level}");
+                        stop_level = t.Split('\\').Length;
+                        Console.WriteLine($"There were {potential_executables.Count} executables found.");
+                        foreach(var exe in potential_executables)
+                        {
+                            Console.WriteLine($"GAME | {exe}");
+                        }
+                    }
+                }
+
+           }
+
+        }
+
+
+        //root_paths.Add(Path.GetPathRoot(Item));
+        //Console.WriteLine(Item);
+       
+
+        Console.ReadLine();
+    }
+
+    public static string[] Walk(string path)
+    {
+        string[] directories = Directory.GetDirectories(path);
+        string[] files = Directory.GetFiles(path);
+        var list = directories.Concat(files).ToArray();
+
+        return list;
+    }
+}
---
+++ dnc8_test/ImportTester/executable.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlas
+{
+    public static class Executable
+    {
+        static bool skip_x86 = true;
+        static string[] blacklist = [
+            "UnityCrashHandler64.exe",
+            "UnityCrashHandler32.exe",
+            "payload.exe",
+            "nwjc.exe",
+            "notification_helper.exe",
+            "nacl64.exe",
+            "chromedriver.exe",
+            "Squirrel.exe",
+            "zsync.exe",
+            "zsyncmake.exe",
+            "cmake.exe",
+            "pythonw.exe",
+            "python.exe",
+            "dxwebsetup.exe",
+            "README.html",
+            "manual.htm",
+            "unins000.exe",
+            "UE4PrereqSetup_X64.exe",
+            "UEPrereqSetup_x64.exe",
+            "credits.html",
+            "LICENSES.chromium.html",
+            "Uninstall.exe",
+            "CONFIG_dl.exe"];
+        static string[] lin_exe = [".sh"];
+        static string[] win_exe = [".exe"];
+        static string[] osx_exe = [".dmg"];
+        static string[] oth_exe = [".swf", ".flv", ".f4v", ".rag", ".cmd", ".bat", ".jar", ".html"];
+
+        public static List<string> DetectExecutable(string[] files) 
+        {
+            string[] extensions = GetExtensions();
+            List<string> potential_executables = []; // This has to reset every time we change folders   
+            foreach(var file in files)
+            {
+                if (File.Exists(file))
+                {
+                    string filename = Path.GetFileName(file);
+                    foreach (var extension in extensions)
+                    {
+                        if (filename.ToLower().Trim().Contains(extension.ToLower().Trim()))
+                        {
+                            //check if file is in blacklist and skip
+                            if (!blacklist.Any(x => x == filename))// .Any(s => s.Equals(filename)));
+                            {
+                                //check if we skip 32bit version of files
+                                if (skip_x86 && filename.Contains("-32")) break;
+
+                                potential_executables.Add(file);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            //!!!NOTE Now that we have all executables, check if there is an exe. If there is, remove everything else !!!
+            // NEEDS TO BE TESTED
+            if (potential_executables.Count > 0)
+            {                
+                if(potential_executables.Any(s => s.EndsWith(".exe")))
+                {
+                    potential_executables.RemoveAll(x => !x.Contains(".exe"));
+                }
+                return potential_executables;   
+            }
+            else
+            {
+                return [];
+            }
+        }
+
+        static string[] GetExtensions()
+        {
+            //combine all extensions
+            //if settings.os == "Windows":
+            return [.. win_exe, .. oth_exe];
+            //if (settings.os == "Linux"):
+            //return executable.lin_exe + executable.oth_exe
+        }
+    }
+}
