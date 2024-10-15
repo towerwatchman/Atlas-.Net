3391a18577faee45d8b88d39c1eb991defcfafa4
towerwatchman <towerwatchman@gmail.com>
2024-10-15 19:16:57 -0400

    skip over downloading images if one already exist.  add temp hack to clear memory

--- Atlas/MainWindow.xaml.cs
+++ Atlas/MainWindow.xaml.cs
@@ -219,29 +219,35 @@ namespace Atlas
                         {
                             try
                             {
-                                //game.F95ID =  SQLiteInterface.FindF95ID(Convert.ToInt32(game.AtlasID)).ToString();
-                                string bannerUrl = SQLiteInterface.GetBannerUrl(game.AtlasID);
-
-                                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "data\\images", game.RecordID.ToString()));
-                                string banner_path = Path.Combine(Directory.GetCurrentDirectory(), "data\\images", game.RecordID.ToString(), Path.GetFileName(bannerUrl));
-                                Atlas.Core.Network.NetworkInterface networkInterface = new Core.Network.NetworkInterface();
-                                await networkInterface.DownloadFileAsync(bannerUrl, banner_path);
-                                //update banner table
-                                if (bannerUrl != "")
+                                //check if banner already exist
+                                if (SQLiteInterface.GetBannerPath(game.RecordID.ToString()) == string.Empty)
                                 {
-                                    SQLiteInterface.UpdateBanners(game.RecordID, banner_path, "banner");
+                                    string bannerUrl = SQLiteInterface.GetBannerUrl(game.AtlasID);
+
+                                    Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "data\\images", game.RecordID.ToString()));
+                                    string banner_path = Path.Combine(Directory.GetCurrentDirectory(), "data\\images", game.RecordID.ToString(), Path.GetFileName(bannerUrl));
+                                    Atlas.Core.Network.NetworkInterface networkInterface = new Core.Network.NetworkInterface();
+                                    await networkInterface.DownloadFileAsync(bannerUrl, banner_path);
+                                    //update banner table
+                                    if (bannerUrl != "")
+                                    {
+                                        SQLiteInterface.UpdateBanners(game.RecordID, banner_path, "banner");
+                                    }
+                                    //game.ImageData = LoadImage(banner_path);
+
+                                    Logging.Logger.Info(game.Title.ToString());
+                                    GameList[GameList.FindIndex(x => x.RecordID == game.RecordID)].ImageData = ImageInterface.LoadImage(bannerUrl == "" ? "" : banner_path, (double)Application.Current.Resources["bannerX"], (double)Application.Current.Resources["bannerY"]);
+
+                                    //replace game item with new data
+
+                                    Application.Current.Dispatcher.Invoke(() =>
+                                    {
+                                        this.BannerView.Items.Refresh();
+                                    });
+                                    //Hack to free up memory
+                                    GC.Collect();
+                                    GC.WaitForPendingFinalizers();
                                 }
-                                //game.ImageData = LoadImage(banner_path);
-
-                                Logging.Logger.Info(game.Title.ToString());
-                                GameList[GameList.FindIndex(x => x.RecordID == game.RecordID)].ImageData = ImageInterface.LoadImage(bannerUrl == "" ? "" : banner_path, (double)Application.Current.Resources["bannerX"], (double)Application.Current.Resources["bannerY"]);
-
-                                //replace game item with new data
-
-                                Application.Current.Dispatcher.Invoke(() =>
-                                {
-                                    this.BannerView.Items.Refresh();
-                                });
                             }
                             catch(Exception ex)
                             {
